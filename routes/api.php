312f1a0b9cd2e6ff32afdf405db4149a10be63aa
Ing. Jose Mella Garcia <lic.mella@gmail.com>
2022-11-01 21:53:33 -0400

    Finish crud books with TDD

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('books', BookController::class);
---
+++ tests/Feature/BookApiTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Book;
+use Tests\TestCase;
+
+class BooksApiTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    function can_get_all_books()
+    {
+        $books = Book::factory(4)->create();
+
+        $this->getJson(route('books.index'))
+            ->assertJsonFragment([
+                'title' => $books[0]->title,
+            ]);
+    }
+
+    /** @test */
+    function can_get_one_book()
+    {
+        $book = Book::factory()->create();
+
+        $this->getJson(route('books.show', $book))
+            ->assertJsonFragment([
+                'title' => $book->title
+            ]);
+    }
+
+    /** @test */
+    function can_create_books()
+    {
+        $this->postJson(route('books.store'), [])
+            ->assertJsonValidationErrorFor('title');
+
+        $this->postJson(route('books.store'), [
+            'title' => 'My new book'
+        ])->assertJsonFragment([
+            'title' => 'My new book'
+        ]);
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'My new book'
+        ]);
+    }
+
+    /** @test */
+    function can_update_books()
+    {
+        $book = Book::factory()->create();
+
+        $this->patchJson(route('books.update', $book), [])
+            ->assertJsonValidationErrorFor('title');
+
+        $this->patchJson(route('books.update', $book), [
+            'title' => 'Edited book'
+        ])->assertJsonFragment([
+            'title' => 'Edited book'
+        ]);
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'Edited book'
+        ]);
+    }
+
+    /** @test */
+    function can_delete_books()
+    {
+        $book = Book::factory()->create();
+
+        $this->deleteJson(route('books.destroy', $book))
+            ->assertNoContent();
+
+        $this->assertDatabaseCount('books', 0);
+    }
+}
